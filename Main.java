7716b031b785c05b1fd736b9dea6bdc4b7f8a940
Mohammad Hossein Dehghani <mhda1380@gmail.com>
2023-11-18 23:23:49 +0330

    program logic corrected, comments added, code cleaned.

--- Main.java
+++ Main.java
@@ -2,7 +2,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Scanner;
@@ -11,22 +10,26 @@ import java.util.Map;
 
 public class Main {
 
+    // Searchs a word in inverted index then returns name of files that are containing that word
     public static Set<Integer> search_word(String word, Map<String, Set<Integer>> invertedIndex) {
         return invertedIndex.getOrDefault(word, Collections.emptySet());
     }
 
     public static void main(String[] args) {
-        ArrayList<Entry_Structure> words_list = new ArrayList<Entry_Structure>();
+        ArrayList<Entry_Structure> words_list = new ArrayList<Entry_Structure>();   // An array list for storing words and file name
 
+        // Getting user input
         Scanner myScanner = new Scanner(System.in);
         String user_input = myScanner.nextLine();
 
+        // Splitting user input to undrestand the command
         String[] user_input_words = user_input.split(" ");
 
         ArrayList<String> necessary_words = new ArrayList<String>();
         ArrayList<String> or_words = new ArrayList<String>();
         ArrayList<String> not_words = new ArrayList<String>();
 
+        // Categorizing command words into or_words, necessary and forbidden groups
         for (String word : user_input_words) {
             if (word.startsWith("+")) {
                 word = word.replace('+', ' ');
@@ -42,8 +45,10 @@ public class Main {
         }
         myScanner.close();
 
+        // Finding name of all files that are in EnglishData directory
         ArrayList<String> file_names_in_folder = FileReader.getFileNames("./EnglishData/");
 
+        // Reading files that are in EnglishData directory
         for (String path : file_names_in_folder) {
             try {
                 File file = new File("./EnglishData/" + path);
@@ -57,25 +62,26 @@ public class Main {
                     fileName.add(Integer.parseInt(path));
 
                     for (String word : splitted_data) {
+                        // Ignoring empty words
                         if (word == "") {
                             continue;
                         }
                         Entry_Structure newEntry = new Entry_Structure(word, fileName);
+                        // Inserting newEntry into words_list
                         words_list.add(newEntry);
                     }
                 }
                 myReader.close();
-
             } catch (FileNotFoundException e) {
                 System.out.println("An error occurred.");
                 e.printStackTrace();
             }
         }
 
-        Collections.sort(words_list, Comparator.comparing(Entry_Structure::getWord));
-
+        // Initializing inverted index table
         Map<String, Set<Integer>> invertedIndex = new HashMap<>();
 
+        // Constructing inverted index table
         for (Entry_Structure entry : words_list) {
             String word = entry.getWord();
             Set<Integer> documentIds = entry.getFile_names();
@@ -83,30 +89,40 @@ public class Main {
             invertedIndex.computeIfAbsent(word, k -> new HashSet<>()).addAll(documentIds);
         }
 
+        // Initializing some sets for forming final answer
         Set<Integer> necessary_files = new HashSet<Integer>();
         Set<Integer> forbidden_files = new HashSet<Integer>();
-        Set<Integer> optional_files = new HashSet<Integer>();
+        Set<Integer> or_files = new HashSet<Integer>();
 
+        // Finding intersection between necessary words' file names
         for (String n_word : necessary_words) {
             Set<Integer> result = search_word(n_word, invertedIndex);
-            necessary_files.addAll(result);
+            if(necessary_files.isEmpty()){
+                necessary_files.addAll(result);
+            }
+            else{
+                necessary_files.retainAll(result);
+            }
         }
 
+        // Finding all or_words' file names
         for (String or_word : or_words) {
             Set<Integer> result = search_word(or_word, invertedIndex);
-            optional_files.addAll(result);
+            or_files.addAll(result);
         }
 
+        // Finding all forbidden words' file names
         for (String not_word : not_words) {
             Set<Integer> result = search_word(not_word, invertedIndex);
             forbidden_files.addAll(result);
         }
 
+        // Final answer is intersection between necessary files and or_files - forbidden files
         Set<Integer> intersection = new HashSet<>(necessary_files);
-        intersection.addAll(optional_files);
-
+        intersection.retainAll(or_files);
         intersection.removeAll(forbidden_files);
 
+        //Printing the result
         for (Integer entry : intersection) {
             System.out.println(entry);
         }
