147aa3bc9ee67ec442407fe51028c976ac440530
Mohammad Hossein Dehghani <mhda1380@gmail.com>
2023-11-23 09:37:56 +0330

    main class added.

--- Phase01-Search/EntryStructure.java
+++ Phase01-Search/EntryStructure.java
@@ -3,18 +3,18 @@ import java.util.Set;
 
 public class EntryStructure {
     private String word;
-    private Set<Integer> fileNames = new HashSet<Integer>();
+    private Set<String> fileNames = new HashSet<String>();
 
-    public EntryStructure(String word, Set<Integer> file_names) {
+    public EntryStructure(String word, Set<String> fileNames) {
         this.word = word;
-        this.fileNames = file_names;
+        this.fileNames = fileNames;
     }
 
     public String getWord() {
         return word;
     }
 
-    public Set<Integer> getFile_names() {
+    public Set<String> getFileNames() {
         return fileNames;
     }
 }
