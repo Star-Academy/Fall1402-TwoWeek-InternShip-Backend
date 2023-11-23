cefc7ec55f259a08fbb5800c319229badac0fbef
Mohammad Hossein Dehghani <mhda1380@gmail.com>
2023-11-23 10:41:56 +0330

    lombok package added to project

--- phase01_search/src/main/java/com/example/EntryStructure.java
+++ phase01_search/src/main/java/com/example/EntryStructure.java
@@ -2,21 +2,16 @@ package com.example;
 
 import java.util.HashSet;
 import java.util.Set;
+import lombok.Getter;
 
 public class EntryStructure {
+    @Getter
     private String word;
+    @Getter
     private Set<String> fileNames = new HashSet<String>();
 
     public EntryStructure(String word, Set<String> fileNames) {
         this.word = word;
         this.fileNames = fileNames;
-    }
-
-    public String getWord() {
-        return word;
-    }
-
-    public Set<String> getFileNames() {
-        return fileNames;
     }
 }
